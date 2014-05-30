55ac37588d2fd5cb10f7aacabfce25ea7da315d9
lafluedecologne <cd@SF.local>
2014-05-30 16:53:17 +0200

    2014-05-30b

---
+++ application/libraries/Frontend_Controller.php
@@ -0,0 +1,10 @@
+<?php
+class Frontend_Controller extends MY_Controller
+{
+    function __construct()
+    {
+        var_dump('Welcome from Frontend Controller');
+        parent::__construct();
+        
+    }
+}
